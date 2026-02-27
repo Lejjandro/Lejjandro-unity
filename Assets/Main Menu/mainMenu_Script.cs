69cc495f8599fa4f90d94c3fe0a3b33975107135
rymd-spel <ht24lego@axevalla.fhsk.se>
2026-02-27 11:39:37 +0100

    a

--- Assets/Main Menu/mainMenu_Script.cs
+++ Assets/Main Menu/mainMenu_Script.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class mainMenu_Script : MonoBehaviour
@@ -11,11 +12,17 @@ public class mainMenu_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
-    
-    public void Playgame()
+
+    public void PlayGame()
     {
+         StartCoroutine(Playgame());
+    }
+
+    IEnumerator Playgame()
+    {
+        yield return new WaitForSeconds(0.1f);
         SceneManager.LoadSceneAsync (1);
     }
 }
